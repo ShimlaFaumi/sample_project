7e080602b3a4cc3325bafcd7cb924a7438db8cc9
ShimlaFaumi <shimlafaume@gmail.com>
2022-08-03 01:15:07 +0530

    initial commit

---
+++ view/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login Page</title>
+        <link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.js">      
+    </head>
+    <body>
+      <div class="container">
+          <div>
+              <div class="col-md-6" style="background-color:grey; height:500px">
+                  &nbsp;
+              </div>
+           </div>
+      </div>
+      <div class="col-md-6" style="background-color:yellow; height:500px">
+      <div class="panel" style="height:300px; margin-top: 90px">
+      <div class="panel-body">
+         <h5>Please sign into your account</h5>
+      <div class="row">
+      <div class="col-md-6">
+         <input type="text" class="form-control"/>
+             </div>
+        </div>
+       </div>
+      </div>
+     </div>
+    </body>   
+</html>
---
+++ view/login2.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login Page</title>
+        <link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css"/>      
+    </head>
+    <body>
+      <div class="container">
+              <div class="col-md-6" style="background-color:#ccc; height:550px">
+                <img src="..images/" style="margin-top:100px"
+                     height="300px" width="100%" align="right"/>
+     </div>
+      <div class="col-md-6" style="background-color:#ccc; height:550px">
+      <div class="panel" style="height:300px; margin-top:100px; border-radius:0 50px 50px 0">
+          &nbsp;
+      <div class="panel-heading">
+         <h4>Please sign into your account</h4>
+      </div>
+          <div class="panel-body">
+      <div class="row">
+      <div class="col-md-6">
+                  <div class="input-group"><span class="input-group-addon">
+              <span class="glyphicon glyphicon-envelope"> </span>
+              </span>
+                  <input type="text" class="form-control" placeholder="Username"/>
+                  </div>
+             </div>
+      </div>
+                   <div class="row">
+                       <div class="col-md-12">
+                           &nbsp;
+                  </div>
+          <div class="row">
+          <div class="col-md-6">
+              <div class="input-group">
+                  <span class="input-group-addon">
+                  <span class="glyphicon glyphicon-lock"> </span>
+                  </span>
+         <input type="password" class="form-control" placeholder="Password"/>
+             </div>
+               <div class="row">
+                       <div class="col-md-12">
+                           &nbsp;
+                  </div>
+        </div>
+              <div class="row">
+              <div class="col-md-6">
+                  <button type="submit" class="btn btn-success
+                          btn-block" style="background-color: #5f05a3;">
+                      Login
+                  </button> 
+              </div>
+       </div>
+      </div>
+     </div>
+    </body>   
+</html>
+
